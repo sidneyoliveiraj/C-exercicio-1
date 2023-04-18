ee5134b98ee0eb4fb78ca2eafd996fff5ab50cdc
Sidney C de Oliveira Jr <sidneycardosojo@gmail.com>
2023-04-17 23:42:02 -0300

    Exercicio 2 resolvido

---
+++ Exercicio 2/Exercicio2.c
@@ -0,0 +1,20 @@
+/*
+Construir um programa que mostre o ano que voce nasceu.
+Deve informar: o ano atual e sua idade.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+//Variaveis
+int idade, ano_nascimento, ano_atual;
+
+//Qual é o processamento de dados?
+ano_nascimento = ano_atual - idade;
+
+//Qual é a saida de dado ?
+printf("O ano que voce nasceu e %i",ano_nascimento);
+
+return 0;
+}
